74697d37087bb2df47eacfb314e45e1519912f99
karishmaaaaa <karishma.ss@kalvium.community>
2024-07-29 10:26:14 +0530

    array of object added

--- erm.cpp
+++ erm.cpp
@@ -42,17 +42,37 @@ public:
 
 
 int main(){
-    Event evnt1;
-    evnt1.setDate("27-4-2024");
-    evnt1.setName("karishma");
-    evnt1.setLocation("conference hall");
-    evnt1.setEventID("EV01");
+    // Event evnt1;
+    // evnt1.setDate("27-4-2024");
+    // evnt1.setName("karishma");
+    // evnt1.setLocation("conference hall");
+    // evnt1.setEventID("EV01");
 
-    cout << evnt1.getDate();
-    cout << evnt1.getName();
-    cout << evnt1.getLocation();
-    cout << evnt1.getEventID();
+    // cout << evnt1.getDate();
+    // cout << evnt1.getName();
+    // cout << evnt1.getLocation();
+    // cout << evnt1.getEventID();
 
+    Event events[2];
+
+    events[0].setDate("27-4-2024");
+    events[0].setName("Karishma's Event");
+    events[0].setLocation("Conference Hall");
+    events[0].setEventID("EV01");
+
+    events[1].setDate("28-4-2024");
+    events[1].setName("Tech Conference");
+    events[1].setLocation("Main Auditorium");
+    events[1].setEventID("EV02");
+
+    for (int i = 0; i < 2; ++i) {
+        cout << "Event " << (i + 1) << " Details:" << endl;
+        cout << "ID: " << events[i].getEventID() << endl;
+        cout << "Name: " << events[i].getName() << endl;
+        cout << "Date: " << events[i].getDate() << endl;
+        cout << "Location: " << events[i].getLocation() << endl;
+        cout << endl;
+    }
     
     Participant par1;
     par1.setParticipantID("27-4-2024");
