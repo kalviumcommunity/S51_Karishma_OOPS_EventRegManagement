4fc6bfa7b050d8004b76e49014c5d58477778ede
karishmaaaaa <karishma.ss@kalvium.community>
2024-09-05 09:20:32 +0530

    setter and getter added

--- erm.cpp
+++ erm.cpp
@@ -19,11 +19,13 @@ public:
         eventCount--;
     }
 
+    // Mutators (Setters)
     void setEventID(const string& id) { this->eventID = id; }
     void setName(const string& n) { this->name = n; }
     void setDate(const string& d) { this->date = d; }
     void setLocation(const string& loc) { this->location = loc; }
 
+    // Accessors (Getters)
     string getEventID() const { return eventID; }
     string getName() const { return name; }
     string getDate() const { return date; }
@@ -53,10 +55,12 @@ public:
         participantCount--;
     }
 
+    // Mutators (Setters)
     void setParticipantID(const string& id) { this->participantID = id; }
     void setName(const string& n) { this->name = n; }
     void registerForEvent() { this->isRegistered = true; }
 
+    // Accessors (Getters)
     string getParticipantID() const { return participantID; }
     string getName() const { return name; }
     bool getRegistrationStatus() const { return isRegistered; }
@@ -71,16 +75,18 @@ int Participant::participantCount = 0;
 int main() {
     Event* events = new Event[2];
 
-    events[0].setDate("27-4-2024");
-    events[0].setName("Karishma's Event");
-    events[0].setLocation("Conference Hall");
+    // Using mutators to set event details
     events[0].setEventID("EV01");
+    events[0].setName("Karishma's Event");
+    events[0].setDate("27-4-2024");
+    events[0].setLocation("Conference Hall");
 
-    events[1].setDate("28-4-2024");
-    events[1].setName("Tech Conference");
-    events[1].setLocation("Main Auditorium");
     events[1].setEventID("EV02");
+    events[1].setName("Tech Conference");
+    events[1].setDate("28-4-2024");
+    events[1].setLocation("Main Auditorium");
 
+    // Using accessors to get event details
     for (int i = 0; i < 2; ++i) {
         cout << "Event " << (i + 1) << " Details:" << endl;
         cout << "ID: " << events[i].getEventID() << endl;
@@ -96,10 +102,12 @@ int main() {
 
     Participant* par1 = new Participant;
 
+    // Using mutators to set participant details
     par1->setParticipantID("P01");
     par1->setName("Karishma");
     par1->registerForEvent();
 
+    // Using accessors to get participant details
     cout << "Participant ID: " << par1->getParticipantID() << endl;
     cout << "Name: " << par1->getName() << endl;
     cout << "Registered: " << (par1->getRegistrationStatus() ? "Yes" : "No") << endl;
