0253d3e0e76bc64a05c6133d7f28e57c381c2e21
karishmaaaaa <141898539+karishmaaaaa@users.noreply.github.com>
2024-11-05 09:31:23 +0530

    Update erm.cpp

--- erm.cpp
+++ erm.cpp
@@ -1,71 +1,78 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 using namespace std;
 
-class EventDetails {
+// Event interface
+class EventInterface {
 public:
-    virtual void displaySpecificDetails() const = 0;
-    virtual ~EventDetails() = default;
+    virtual void displayDetails() const = 0;
+    virtual ~EventInterface() = default;
 };
 
-class SportsDetails : public EventDetails {
-private:
-    string sportType;
-
-public:
-    SportsDetails(const string& type) : sportType(type) {}
-
-    void displaySpecificDetails() const override {
-        cout << "Sport Type: " << sportType << endl;
-    }
-};
-
-class CulturalDetails : public EventDetails {
-private:
-    string theme;
-
-public:
-    CulturalDetails(const string& t) : theme(t) {}
-
-    void displaySpecificDetails() const override {
-        cout << "Theme: " << theme << endl;
-    }
-};
-
-class Event {
+// SportsEvent class implementing EventInterface
+class SportsEvent : public EventInterface {
 private:
     string eventID;
     string name;
     string date;
     string location;
-    unique_ptr<EventDetails> details;
+    string sportType;
 
 public:
-    static int eventCount;
+    SportsEvent(const string& id, const string& n, const string& d, const string& loc, const string& type)
+        : eventID(id), name(n), date(d), location(loc), sportType(type) {}
 
-    // Parameterized constructor
-    Event(const string& id, const string& n, const string& d, const string& loc, unique_ptr<EventDetails> det) 
-        : eventID(id), name(n), date(d), location(loc), details(move(det)) {
-        eventCount++;
-    }
-
-    // Destructor
-    virtual ~Event() { eventCount--; }
-
-    // Display event details
-    void displayDetails() const {
-        cout << "Event ID: " << eventID << endl;
+    void displayDetails() const override {
+        cout << "Sports Event Details:" << endl;
+        cout << "ID: " << eventID << endl;
         cout << "Name: " << name << endl;
         cout << "Date: " << date << endl;
         cout << "Location: " << location << endl;
-        details->displaySpecificDetails();  // Display specific event type details
+        cout << "Sport Type: " << sportType << endl;
     }
-
-    // Static function to get the total count of events
-    static int getEventCount() { return eventCount; }
 };
 
-int Event::eventCount = 0;
+// CulturalEvent class implementing EventInterface
+class CulturalEvent : public EventInterface {
+private:
+    string eventID;
+    string name;
+    string date;
+    string location;
+    string theme;
+
+public:
+    CulturalEvent(const string& id, const string& n, const string& d, const string& loc, const string& t)
+        : eventID(id), name(n), date(d), location(loc), theme(t) {}
+
+    void displayDetails() const override {
+        cout << "Cultural Event Details:" << endl;
+        cout << "ID: " << eventID << endl;
+        cout << "Name: " << name << endl;
+        cout << "Date: " << date << endl;
+        cout << "Location: " << location << endl;
+        cout << "Theme: " << theme << endl;
+    }
+};
+
+// EventManager class depends on the EventInterface, not specific event types
+class EventManager {
+private:
+    vector<unique_ptr<EventInterface>> events;
+
+public:
+    void addEvent(unique_ptr<EventInterface> event) {
+        events.push_back(move(event));
+    }
+
+    void displayAllEvents() const {
+        for (const auto& event : events) {
+            event->displayDetails();
+            cout << endl;
+        }
+    }
+};
 
 class Participant {
 private:
@@ -73,7 +80,7 @@ private:
     string name;
 
 public:
-    Participant(const string& id, const string& n) 
+    Participant(const string& id, const string& n)
         : participantID(id), name(n) {}
 
     // Accessors (Getters)
@@ -108,24 +115,20 @@ public:
 int ParticipantManager::participantCount = 0;
 
 int main() {
-    // Creating different types of events using the open/closed principle
-    Event event1("SE03", "Tennis Match", "1-5-2024", "Court A", make_unique<SportsDetails>("Tennis"));
-    Event event2("CE01", "Art Exhibition", "2-5-2024", "Art Hall", make_unique<CulturalDetails>("Modern Art"));
+    EventManager eventManager;
 
-    // Display details for each event
-    event1.displayDetails();
-    cout << endl;
-    event2.displayDetails();
-    cout << endl;
+    // Adding different types of events to EventManager
+    eventManager.addEvent(make_unique<SportsEvent>("SE03", "Tennis Match", "1-5-2024", "Court A", "Tennis"));
+    eventManager.addEvent(make_unique<CulturalEvent>("CE01", "Art Exhibition", "2-5-2024", "Art Hall", "Modern Art"));
 
-    // Display total number of events
-    cout << "Total Events: " << Event::getEventCount() << endl;
+    // Display all events through EventManager
+    eventManager.displayAllEvents();
 
     // Participant and ParticipantManager example
-    ParticipantManager manager;
+    ParticipantManager participantManager;
     Participant* par1 = new Participant("P01", "Karishma");
-    manager.addParticipant(par1);
-    manager.registerParticipant(par1);
+    participantManager.addParticipant(par1);
+    participantManager.registerParticipant(par1);
     cout << "Participant ID: " << par1->getParticipantID() << endl;
     cout << "Name: " << par1->getName() << endl;
     cout << "Total Participants: " << ParticipantManager::getParticipantCount() << endl;
